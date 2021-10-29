9a2283548cea9180aa8b4cbb405927d4c5e86004
Artur <sitdikov95@list.ru>
2021-10-29 18:22:32 +0300

    HW1 commit

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -1,53 +1,69 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <strstream>
+#include <cmath>
 
 using namespace std;
 
+int add_memory(int* array){
+    int array_size = sizeof array;
+    int* new_array = new int[2 * array_size];
+    memcpy(new_array, array, array_size);
+    return *new_array;
+}
+
 int main() {
-    ifstream file("C:\\Users\\Artur\\CLionProjects\\task1\\in.txt");
+    ifstream file("in.txt");
+
     string line;
-    int k = 0;
+
+    int* arr = new int[100];
+    int i = 0;
 
     if(file.is_open()){
-        while(getline(file, line)){
-            k++;
-        }
-
-        int* arr = new int[k];
-
-        int j = 0;
-        while(getline(file, line)){
-            char tmp[line.size()];
-            strstream x;
-            x << line;
-            while(x >> tmp){
-                arr[j] = stoi(tmp);
-                j = j + 1;
+        while(!file.eof()){
+            int tmp;
+            file >> tmp;
+            arr[i] = tmp;
+            i = i + 1;
+            if(i != 0 && i % 10 == 0){
+                *arr = add_memory(arr);
             }
         }
-
-        int n = sizeof(*arr) / sizeof(arr[0]);
-        int* arr_x = new int[k / 2];
-        int* arr_y = new int[k / 2];
-
-        int x = 0;
-        int y = 0;
-        for(int i = 0; i < n; i++){
-            if(i % 2 == 0){
-                arr_x[x] = arr[i];
-                x = x + 1;
-            }else{
-                arr_y[y] = arr[i];
-                y = y + 1;
-            }
-        }
-
-
-
     }
     file.close();
 
+    int x_n = arr[0];
+    int y_n = arr[1];
+    float l_n = float(sqrt(x_n * x_n + y_n * y_n));
+    int left[2];
+    int right[2];
+    float length_left = 0;
+    float length_right = 0;
+
+    for(int k = 3; k < i; k = k + 2){;
+        int vec_multi = x_n * arr[k] - y_n * arr[k - 1];
+        if(vec_multi > 0){
+            float tmp_length;
+            tmp_length = float(vec_multi) / l_n;
+            if(length_left < tmp_length){
+                length_left = tmp_length;
+                left[0] = arr[k - 1];
+                left[1] = arr[k];
+            }
+        }else{
+            float tmp_length;
+            tmp_length = (-1) * float(vec_multi) / l_n;
+            if(length_right < tmp_length){
+                length_right = tmp_length;
+                right[0] = arr[k - 1];
+                right[1] = arr[k];
+            }
+        }
+    }
+
+    delete[] arr;
+
+    cout << "Leftmost: " << left[0] << " " << left[1] << endl;
+    cout << "Rightmost: " << right[0] << " " << right[1];
     return 0;
 }
