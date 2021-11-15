badea1d73c56fb12f56ad2921330eaa7cf8a8113
Artur <sitdikov95@list.ru>
2021-11-15 21:10:46 +0300

    Update

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -43,8 +43,8 @@ int main(int argc, char* argv[]) {
     h0 = arr[0];
     v_x = arr[1];
     v_y = arr[2];
-    int arr_x[(i - 4) / 2];
-    int arr_h[(i - 4) / 2];
+    int arr_x[(i - 3) / 2];
+    int arr_h[(i - 3) / 2];
 
     int k = 0;
     int n = 0;
@@ -52,7 +52,7 @@ int main(int argc, char* argv[]) {
         arr_x[k] = arr[j];
         k = k + 1;
     }
-    for(int j = 4; j < (i - 1); j = j + 2){
+    for(int j = 4; j <= (i - 1); j = j + 2){
         arr_h[n] = arr[j];
         n = n + 1;
     }
@@ -63,8 +63,9 @@ int main(int argc, char* argv[]) {
     double res_x = 0;
     k = 0;
     bool flag = false;
-    while(k < ((i - 4) / 2)){
+    while(k < ((i - 3) / 2) && k >= 0){
         y = trajectory(x_0, arr_x[k], v_x, v_y, h0);
+        cout << y << endl;
         if(y > 0 && y < arr_h[k]){
             v_x = (-1) * v_x;
             x_0 = arr_x[k] - x_0;
@@ -75,7 +76,11 @@ int main(int argc, char* argv[]) {
             }
             flag = true;
         }else if(y > 0 && y > arr_h[k]){
-            k = k + 1;
+            if(v_x > 0){
+                k = k + 1;
+            }else {
+                k = k - 1;
+            }
             flag = false;
             continue;
         }else{
@@ -97,21 +102,31 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    if(k == ((i - 4) / 2)) {
+    if(k == ((i - 3) / 2)) {
         double *result = end(x_0, v_y, v_x, h0);
         if(v_x < 0){
             res_x = result[0];
         }else{
             res_x = result[1];
         }
+    }else if(k < 0){
+        double *result = end(x_0, v_y, v_x, h0);
+        if(v_x > 0){
+            res_x = result[0];
+        }else{
+            res_x = result[1];
+        }
     }
 
-    if(res_x > arr_x[(i - 4) / 2 - 1]){
-        cout << (i - 4) / 2 - 1;
+    cout << res_x << endl;
+
+    if(res_x > arr_x[(i - 3) / 2]){
+        cout << (i - 3) / 2;
     }else{
-        for(int j = 0; j < (i - 4) / 2; j++){
+        for(int j = 0; j <= (i - 3) / 2; j++){
             if(res_x < arr_x[j]){
                 cout << j;
+                break;
             }
         }
     }
